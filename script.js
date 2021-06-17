fdc6b1859bd0eed3f99116f9fe53ce97c144b289
Balaji Shivankar <shivankar_b@India.XoriantCorp.com>
2021-06-17 14:07:22 +0530

    second day work

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-console.log("welcome")
-console.log(20+4-2)
-console.log("my first programme")
+// console.log("welcome")
+// console.log(20+4-2)
+// console.log("my first programme")
+let fruit= ['orange','apple','banana']
+fruit.pop()
+fruit.push('mango')
+fruit.shift()
+fruit.unshift('PAPAI')
+console.log('length',fruit.length)
+console.log(fruit)
+let arr=['orange','apple','banana']
+for(let i=0;i<arr.length;i++){
+    if(arr[i]=='apple'){
+        console.log('if',arr[i])
+    } else{
+        console.log('else',arr[i])
+    }
+//console.log(arr[i])
+}
